38dce2bcb6af3edd48e61491ea1254ad239f8590
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-10-14 13:23:40 -0300

    [fix] funcion- HATEOAS

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,37 +1,67 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { readInventory, createItem, updateItem, deleteItem } = require('../utils/pg');
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Función HATEOAS
+
+function createHateoasLinks(id) {
+  return [
+    { rel: 'self', href: `/inventory/${id}` },
+    { rel: 'update', href: `/inventory/${id}`, method: 'PUT', title: 'Update Item' },
+    { rel: 'delete', href: `/inventory/${id}`, method: 'DELETE', title: 'Delete Item' }
+  ];
+}
+
 app.get('/inventory', (_, res, next) => {
-    readInventory()
-        .then((items) => res.status(200).json(items))
-        .catch((error) => next(error));
+  readInventory()
+    .then((items) => {
+      // aqui rey HATEOAS a cada elemento
+      const itemsWithHateoas = items.map((item) => ({
+        ...item,
+        links: createHateoasLinks(item.id)
+      }));
+      res.status(200).json(itemsWithHateoas);
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/inventory', (req, res, next) => {
-    const { nombre, categoria, metal, precio, stock } = req.body;
-    createItem({ nombre, categoria, metal, precio, stock })
-        .then((result) => res.status(result?.code ? 500 : 200).json(result))
-        .catch((error) => next(error));
+  const { nombre, categoria, metal, precio, stock } = req.body;
+  createItem({ nombre, categoria, metal, precio, stock })
+    .then((result) => {
+      const item = {
+        ...result,
+        links: createHateoasLinks(result.id)
+      };
+      res.status(result.code ? 500 : 201).json(item);
+    })
+    .catch((error) => next(error));
 });
 
 app.put('/inventory/:id', (req, res, next) => {
-    updateItem(req.params.id, req.body)
-        .then((result) => res.status(result?.code ? 500 : 200).json(result))
-        .catch((error) => next(error));
+  updateItem(req.params.id, req.body)
+    .then((result) => {
+      const item = {
+        ...result,
+        links: createHateoasLinks(req.params.id)
+      };
+      res.status(result.code ? 500 : 200).json(item);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete('/inventory/:id', (req, res, next) => {
-    deleteItem(req.params.id)
-        .then((result) => res.status(result?.code ? 500 : 200).json(result))
-        .catch((error) => next(error));
+  deleteItem(req.params.id)
+    .then((result) => {
+      res.status(result.code ? 500 : 204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.all('*', (_, res) => res.status(404).json({ code: 404, message: 'La ruta no existe' }));
--- backend/utils/pg.js
+++ backend/utils/pg.js
@@ -1,42 +1,74 @@
-require('dotenv').config()
-const { Pool } = require('pg')
-const { v4: uuidv4 } = require('uuid')
+const { Pool } = require('pg');
+const { v4: uuidv4 } = require('uuid');
 
 const config = {
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    allowExitOnIdle: true
-}
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  allowExitOnIdle: true,
+};
 
-const pool = new Pool(config)
+const pool = new Pool(config);
 
-const genericSqlQuery = (query = '', values = []) => pool
+const genericSqlQuery = (query = '', values = []) =>
+  pool
     .query(query, values)
     .then(({ rows }) => rows)
-    .catch(({ code, message }) => ({ code, message }))
+    .catch(({ code, message }) => ({ code, message }));
 
-const readInventory = async () => await genericSqlQuery('SELECT * FROM inventario;')
+const addHateoasLinks = (item) => {
+  item.links = [
+    {
+      rel: 'self',
+      href: `/inventory/${item.id}`,
+    },
+    {
+      rel: 'update',
+      href: `/inventory/${item.id}`,
+      method: 'PUT',
+      title: 'Update Item',
+    },
+    {
+      rel: 'delete',
+      href: `/inventory/${item.id}`,
+      method: 'DELETE',
+      title: 'Delete Item',
+    },
+  ];
+  return item;
+};
+
+const readInventory = async () => {
+  const items = await genericSqlQuery('SELECT * FROM inventario;');
+  return items.map(addHateoasLinks);
+};
 
 const createItem = async ({ nombre, categoria, metal, precio, stock }) => {
-    const query = 'INSERT INTO inventario(id, nombre, categoria, metal, precio, stock) VALUES ($1, $2, $3, $4, $5, $6);'
-    const values = [uuidv4(), nombre, categoria, metal, precio, stock]
-    return await genericSqlQuery(query, values)
-}
+  const query =
+    'INSERT INTO inventario(id, nombre, categoria, metal, precio, stock) VALUES ($1, $2, $3, $4, $5, $6);';
+  const values = [uuidv4(), nombre, categoria, metal, precio, stock];
+  const result = await genericSqlQuery(query, values);
+  return addHateoasLinks(result[0]);
+};
 
 const updateItem = async (id, { nombre, categoria, metal, precio, stock }) => {
-    const query = 'UPDATE inventario SET nombre = $2, categoria = $3, metal = $4, precio = $5, stock = $6 WHERE id = $1 RETURNING *;'
-    const values = [id, nombre, categoria, metal, precio, stock]
-    return await genericSqlQuery(query, values)
-}
+  const query =
+    'UPDATE inventario SET nombre = $2, categoria = $3, metal = $4, precio = $5, stock = $6 WHERE id = $1 RETURNING *;';
+  const values = [id, nombre, categoria, metal, precio, stock];
+  const result = await genericSqlQuery(query, values);
+  return addHateoasLinks(result[0]);
+};
 
-const deleteItem = async (id) => await genericSqlQuery('DELETE FROM inventario WHERE id = $1 RETURNING *;', [id])
+const deleteItem = async (id) => {
+  const result = await genericSqlQuery('DELETE FROM inventario WHERE id = $1 RETURNING *;', [id]);
+  return addHateoasLinks(result[0]);
+};
 
 module.exports = {
-    readInventory,
-    createItem,
-    updateItem,
-    deleteItem
-}
+  readInventory,
+  createItem,
+  updateItem,
+  deleteItem,
+};
